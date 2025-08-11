13bc2a293b4cf5b051f4af18e46d3f749390f186
borisletic <borisletic001@gmail.com>
2025-08-11 18:08:44 +0200

    Spring init

---
+++ pki-system/pki-system/src/main/java/rs/ac/uns/ftn/pkisystem/PkiSystemApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.pkisystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkiSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkiSystemApplication.class, args);
+	}
+
+}
